3b7ac0721c847c8e90faeb7049834be329ff260b
JonasChua <chuashanhong2000@gmail.com>
2023-11-20 13:51:36 +0800

    Updated main, controllers & enums

--- src/main/CAMs.java
+++ src/main/CAMs.java
@@ -2,11 +2,33 @@ package main;
 
 import controllers.SessionController;
 import controllers.DataTransferController;
+import controllers.StaffController;
+import controllers.StudentController;
+import controllers.CommitteeController;
+import interfaces.currentUserDaoInterface;
 
+/**
+ * The main class executing the Camp Application and Management System (CAMs).
+ * This class handles the initialisation of data, log-in and starting the
+ * appropriate session based on the role of logged-in user.
+ * 
+ * @author Chua Shan Hong
+ *
+ */
 public class CAMs {
-
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		do {
+			DataTransferController.importData();
+			SessionController.startSession();
+			if (!currentUserDao)
+			
+			SessionController.endSession();
+			DataTransferController.exportData();
+		} while (true);
 
 	}
 
