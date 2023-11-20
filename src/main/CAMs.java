8dcd367f9abcc2dc65cd25a58a34cd2d213eab55
JonasChua <chuashanhong2000@gmail.com>
2023-11-20 23:15:56 +0800

    Updated main, controllers & enums

--- src/main/CAMs.java
+++ src/main/CAMs.java
@@ -5,7 +5,12 @@ import controllers.DataTransferController;
 import controllers.StaffController;
 import controllers.StudentController;
 import controllers.CommitteeController;
-import interfaces.currentUserDaoInterface;
+
+import enums.Role;
+
+import interfaces.CurrentUserDao;
+
+import models.User;
 
 /**
  * The main class executing the Camp Application and Management System (CAMs).
@@ -21,10 +26,28 @@ public class CAMs {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		do {
 			DataTransferController.importData();
 			SessionController.startSession();
-			if (!currentUserDao)
+			
+			currentUserDao currentUser;
+			try {
+				User user = currentUserDao.getCurrentUser();
+				switch (user.getRole()) {
+				case STAFF:
+					new StaffController().start();
+					break;
+				case STUDENT:
+					new StudentController().start();
+					break;
+				case COMMITTEE:
+					new CommitteeController().start();
+					break;
+				}
+			} catch (Exception e) {
+				break;
+			}
 			
 			SessionController.endSession();
 			DataTransferController.exportData();
