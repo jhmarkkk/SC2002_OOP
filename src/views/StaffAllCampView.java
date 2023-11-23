cb2f98973ccb2a58a8f8d4ea2634c6fa8add1198
JJChong777 <jiejun815@gmail.com>
2023-11-23 09:41:42 +0800

    Update StaffAllCampView.java

--- src/views/StaffAllCampView.java
+++ src/views/StaffAllCampView.java
@@ -3,6 +3,7 @@ package views;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 
 import enums.FilterType;
 import interfaces.views.CampViewable;
@@ -12,22 +13,24 @@ import utils.CampFormatDate;
 
 public class StaffAllCampView implements CampViewable {
     public void filterView(FilterType filterType) {
-        ARrayList<Camp> 
-        ArrayList<Camp> camps = CampFilter.filter(camps, filterType);
+        CampDaoInterface campDao;
+        Map<String, Camp> campsMap = campDao.getCamps();
+        ArrayList<Camp> staffAllCamps = new ArrayList<Camp>(campsMap.values());
+        ArrayList<Camp> staffAllCamps = CampFilter.filter(staffAllCamps, filterType);
         int index = 1;
         SimpleDateFormat formatter = CampFormatDate.dateFormat();
         System.out.println("===== List of Camps : Staff =====");
-        for (Camp camp : ) {
-            System.out.printf("----- (Camp %d) %s -----\n", index, camp.getName());
-            System.out.printf("Visibility : %s", camp.getVisibility().toString());
+        for (Camp staffCamp : staffAllCamps) {
+            System.out.printf("----- (Camp %d) %s -----\n", index, staffCamp.getName());
+            System.out.printf("Visibility : %s", staffCamp.getVisibility().toString());
             System.out.print("Dates: ");
-            for (Date date : camp.getDates()) {
+            for (Date date : staffCamp.getDates()) {
                 System.out.printf("%s ", formatter.format(date));
             }
-            System.out.printf("\nLocation: %s\n", camp.getLocation());
-            System.out.printf("Attendee Slots available: %d\n", camp.getAttendeeSlots());
-            System.out.printf("Camp Committee Slots available: %d\n", camp.getCommitteeSlots());
-            System.out.printf("Staff in charge: %s\n", camp.getStaffInCharge().getName());
+            System.out.printf("\nLocation: %s\n", staffCamp.getLocation());
+            System.out.printf("Attendee Slots available: %d\n", staffCamp.getAttendeeSlots());
+            System.out.printf("Camp Committee Slots available: %d\n", staffCamp.getCommitteeSlots());
+            System.out.printf("Staff in charge: %s\n", staffCamp.getStaffInCharge().getName());
             index++;
         }
     }
