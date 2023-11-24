835de81a9a1e8f9f3f4b011fda2e35080f0e4d69
JJChong777 <jiejun815@gmail.com>
2023-11-25 04:10:15 +0800

    Update StaffReplyEnquiryService.java

--- src/services/StaffReplyEnquiryService.java
+++ src/services/StaffReplyEnquiryService.java
@@ -4,6 +4,8 @@ import interfaces.dao.CampDao;
 import interfaces.dao.CurrentUserDao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import dao.CampDaoImpl;
@@ -14,6 +16,7 @@ import interfaces.services.ReplyEnquiryServiceable;
 
 import models.Staff;
 import models.Enquiry;
+import models.Camp;
 
 public class StaffReplyEnquiryService implements ReplyEnquiryServiceable {
     Scanner sc = new Scanner(System.in);
@@ -25,10 +28,13 @@ public class StaffReplyEnquiryService implements ReplyEnquiryServiceable {
         // downcast user into a staff object
         Staff staff = (Staff) currentUserDao.getCurrentUser();
         ArrayList<Integer> enquiriesID = new ArrayList<Integer>();
+        Map<Integer, Camp> enqIDtoCamp = new HashMap<Integer, Camp>();
         // first, put get all the created camps and for createdCamp, put the enquiry
         // into a arraylist
         for (String createdCampID : staff.getCreatedCamps()) {
-            for (Enquiry enq : campDao.getCamps().get(createdCampID).getEnquiries().values()) {
+            Camp createdCamp = campDao.getCamps().get(createdCampID);
+            for (Enquiry enq : createdCamp.getEnquiries().values()) {
+                enqIDtoCamp.put(enq.getEnquiryID(), campDao.getCamps().get(createdCampID));
                 enquiriesID.add(enq.getEnquiryID());
             }
         }
@@ -37,19 +43,26 @@ public class StaffReplyEnquiryService implements ReplyEnquiryServiceable {
             System.out.println("No enquries to reply to!");
             return;
         }
-        for (i = 0; i < enquiriesID.size(); i++) {
-            System.out.printf("Choice %d : Enquiry ID %d", i + 1, enquiriesID.get(i));
-            System.out.printf("Choice %d : Back", i + 2);
+        do {
+            for (i = 0; i < enquiriesID.size(); i++)
+                System.out.printf("Choice %d : Enquiry ID %d", i, enquiriesID.get(i));
+            System.out.printf("Choice %d : Back", i + 1);
             System.out.printf("Select choice: ");
             choice = sc.nextInt();
             System.out.println();
-            if (choice == i + 2)
+            if (choice == i + 1)
                 return;
-            if (choice >= 1 || choice <= i + 2) {
+            if (choice >= 0 || choice <= i) {
                 enquiryReplyID = enquiriesID.get(i);
                 break;
             }
-        }
-
+        } while (true);
+        System.out.println("Enter your reply: ");
+        String replyString = sc.nextLine();
+        Camp selectedCamp = enqIDtoCamp.get(enquiryReplyID);
+        Enquiry selectedEnquiry = selectedCamp.getEnquiries().get(enquiryReplyID);
+        selectedEnquiry.setReply(replyString);
+        selectedEnquiry.setReplier(staff.getUserID());
+        System.out.printf("Reply sent! (Staff %s)\n", staff.getUserID());
     }
 }
