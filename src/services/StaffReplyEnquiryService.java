a67ade6a3406502d78869c194f361c92998f8756
jchew049 <jchew049@e.ntu.edu.sg>
2023-11-25 00:59:07 +0800

    finish StaffCampService

--- src/services/StaffReplyEnquiryService.java
+++ src/services/StaffReplyEnquiryService.java
@@ -5,5 +5,6 @@ import interfaces.services.ReplyEnquiryServiceable;
 public class StaffReplyEnquiryService implements ReplyEnquiryServiceable {
     public void reply(){
         // TODO Auto-generated method stub
+        
     }
 }
