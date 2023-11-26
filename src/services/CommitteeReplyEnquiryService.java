ed25fa91084b160ad81560a81e578a73f63e977b
JJChong777 <jiejun815@gmail.com>
2023-11-26 15:42:32 +0800

    Update CommitteeReplyEnquiryService.java

--- src/services/CommitteeReplyEnquiryService.java
+++ src/services/CommitteeReplyEnquiryService.java
@@ -26,23 +26,24 @@ public class CommitteeReplyEnquiryService implements ReplyEnquiryServiceable {
     public static final CurrentUserDao currentUserDao = new CurrentUserDaoImpl();
 
     public void reply() {
-        
+
         int i = 0, choice;
         Enquiry selectedEnquiry;
         String replyField;
-        CommitteeMember currentUser = (CommitteeMember)currentUserDao.getCurrentUser();
+        CommitteeMember currentUser = (CommitteeMember) currentUserDao.getCurrentUser();
         String campName = currentUser.getFacilitatingCamp();
         Camp camp = campDao.getCamps().get(campName);
         ArrayList<Enquiry> validEnquiryList = new ArrayList<Enquiry>();
 
         for (Enquiry enquiry : camp.getEnquiries().values()) {
-            if (enquiry.getEnquirer() != null) continue;
+            if (enquiry.getEnquirer() != null)
+                continue;
 
             validEnquiryList.add(enquiry);
         }
 
         if (validEnquiryList.size() == 0) {
-            System.out.println("No enqury to reply");
+            System.out.println("No enquiry to reply");
             return;
         }
 
@@ -52,20 +53,21 @@ public class CommitteeReplyEnquiryService implements ReplyEnquiryServiceable {
 
             System.out.printf("%2d : Back\n", i + 1);
             choice = InputUtil.choice();
-            if (choice == i + 1) return;
-                
+            if (choice == i + 1)
+                return;
+
             if (choice >= 1 || choice <= i) {
                 selectedEnquiry = validEnquiryList.get(choice - 1);
                 break;
             }
         } while (true);
 
-
-        System.out.printf("%-10s: %s\n","Camp" , campName);
-        System.out.printf("%-10s: %s\n","Enquiry" , selectedEnquiry.getEnquiry());
+        System.out.printf("%-10s: %s\n", "Camp", campName);
+        System.out.printf("%-10s: %s\n", "Enquiry", selectedEnquiry.getEnquiry());
         do {
             replyField = InputUtil.nextString("Enter reply");
-            if (!replyField.isBlank()) break;
+            if (!replyField.isBlank())
+                break;
 
             PrintUtil.invalid("input");
         } while (true);
