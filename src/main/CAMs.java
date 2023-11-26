2ce12ed7af46418dbfa0f6ff2e9a205bec504e4d
HCaihong <126852029+HCaihong@users.noreply.github.com>
2023-11-26 14:03:58 +0800

    javadoc for main

--- src/main/CAMs.java
+++ src/main/CAMs.java
@@ -15,6 +15,34 @@ import interfaces.dao.StudentDao;
 import models.Staff;
 import models.User;
 
+/**
+ * The {@code CAMs} class serves as the entry point for the Course Administration Management System (CAMs) application.
+ * It initializes the necessary DAOs and controllers, imports data, and starts user sessions based on their roles.
+ * 
+ * <p>The application supports user roles such as Staff, Student, and Committee, each with its respective controller.</p>
+ * 
+ * <p>The main method controls the flow of the application by importing data, setting the current user, determining the user's role, and initiating the corresponding controller.</p>
+ * 
+ * <p>Note: The application assumes a loop where users can interact with the system until they choose to exit.</p>
+ * 
+ * @author Chuan Shan Hong
+ * @version 1.0
+ * @since 1.0
+ * 
+ * @see SessionController
+ * @see DataTransferController
+ * @see StaffController
+ * @see StudentController
+ * @see CommitteeController
+ * @see CurrentUserDaoImpl
+ * @see StaffDaoImpl
+ * @see StudentDaoImpl
+ * @see interfaces.dao.CurrentUserDao
+ * @see interfaces.dao.StaffDao
+ * @see interfaces.dao.StudentDao
+ * @see models.Staff
+ * @see models.User
+ */
 public class CAMs {
 
 	private static final CurrentUserDao currentUserDao = new CurrentUserDaoImpl();
