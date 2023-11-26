787f4de21f6883884f427a7717643bfc09bdae38
JJChong777 <jiejun815@gmail.com>
2023-11-26 13:38:15 +0800

    Update ProfileView.java

--- src/views/ProfileView.java
+++ src/views/ProfileView.java
@@ -12,26 +12,26 @@ import utils.PrintUtil;
 import models.CommitteeMember;
 
 public class ProfileView implements ProfileViewable {
-	
-	private static final CurrentUserDao currentUserDao = new CurrentUserDaoImpl();
-	
+
+    private static final CurrentUserDao currentUserDao = new CurrentUserDaoImpl();
+
     public void view() {
-    	
+
         User currentUser = currentUserDao.getCurrentUser();
         CommitteeMember committeeMember;
-        
+
         PrintUtil.header("User Profile");
-        System.out.printf("%-10s: %s\n","User ID" , currentUser.getUserID());
+        System.out.printf("%-10s: %s\n", "User ID", currentUser.getUserID());
         System.out.printf("%-10s: %s\n", "Name", currentUser.getName());
-        System.out.printf("%-10s: %s\n", "Format", currentUser.getFaculty());
+        System.out.printf("%-10s: %s\n", "Faculty", currentUser.getFaculty());
         System.out.printf("%-10s: %s", "Role", Role.toString(currentUser.getRole()));
-        
+
         if (currentUser.getRole() == Role.COMMITTEE) {
             committeeMember = (CommitteeMember) currentUser;
             System.out.printf(" of %s\n", committeeMember.getFacilitatingCamp());
             System.out.printf("%-10s: %d\n", "Points", committeeMember.getPoints());
         }
-        
+
         System.out.println("\n");
     }
 }
